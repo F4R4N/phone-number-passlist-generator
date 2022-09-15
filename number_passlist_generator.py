8f72c53ee01473135e1ffadb26d50743a8766198
Faran <farantgh@gmail.com>
2022-09-15 14:12:14 +0430

    rename file to match python module filename convention

---
+++ number_passlist_generator.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from datetime import timedelta
+import time
+from tqdm import trange
+
+
+def human_readable_filesize(num, suffix="B"):
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+def main():
+    prenumber = input("pre-number : ")
+    path = input("enter exact path to save file: ")
+    os.chdir(path)
+    start_time = time.time()
+    print("waiting . . .")
+    filename = 'passlist-' + prenumber + '-py.txt'
+    with open(filename, "a", encoding="utf-8") as file:
+        for i in trange(0, 10000000):
+            i = str(i).zfill(7)
+            res = prenumber + i
+            file.write(res + '\n')
+    file_size = human_readable_filesize(os.path.getsize(filename))
+
+    end_time = time.time()
+    res_time = timedelta(seconds=int(end_time - start_time))
+    print(f"Phone numbers saved to '{filename}'\nFilesize: '{file_size}'\nDuration: '{res_time}s'")
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("you used Ctrl+C\nexiting . . . ")
+        sys.exit(0)
